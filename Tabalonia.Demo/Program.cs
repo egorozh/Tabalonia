792378581a55da76ec5e94f4a3c6defcde0699a9
Егор Желудков <zheludkov@cmsplanet.ru>
2023-01-24 22:11:21 +0300

    Removed old demo

--- Tabalonia.Demo/Program.cs
+++ Tabalonia.Demo/Program.cs
@@ -1,18 +1,21 @@
-﻿using System;
-using Avalonia;
-using Avalonia.ReactiveUI;
+﻿using Avalonia;
+using System;
 
-namespace Tabalonia.Demo;
-
-internal class Program
+namespace TabaloniaNew.Demo
 {
-    [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
-    
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
-            .UsePlatformDetect()
-            .LogToTrace()
-            .UseReactiveUI();
+    class Program
+    {
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        [STAThread]
+        public static void Main(string[] args) => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToTrace();
+    }
 }
