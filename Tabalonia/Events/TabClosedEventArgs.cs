5d77b18121ea84c5ad6c0c497883fdaa0b8e0d1c
zheludkov <zheludkov@ddplanet.ru>
2025-07-21 12:01:16 +0300

    fix: removed nullable annotation

--- Tabalonia/Events/TabClosedEventArgs.cs
+++ Tabalonia/Events/TabClosedEventArgs.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tabalonia.Events;
 
-public class TabClosedEventArgs(object? item) : EventArgs
+public class TabClosedEventArgs(object item) : EventArgs
 {
-    public object? Item { get; } = item;
+    public object Item { get; } = item;
 }
